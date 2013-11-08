2b56facb8c0174bdc49c4074985e9e284adb843f
Johan Stjernberg <jostjernberg@gmail.com>
2013-11-08 14:04:49 +0100

    Added JUnit test structure with stub files.

---
+++ test/kth/game/othello/OthelloTest.java
@@ -0,0 +1,18 @@
+package kth.game.othello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OthelloTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
