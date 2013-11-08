377cf171cb0689276f836446828336fa200c97ec
Johan Stjernberg <jostjernberg@gmail.com>
2013-11-08 16:20:36 +0100

    Dev; placeholder node test file added.

---
+++ test/kth/game/othello/board/ClassicNodeTest.java
@@ -0,0 +1,39 @@
+package kth.game.othello.board;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ClassicNodeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testClassicNode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOccupantPlayerId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetXCoordinate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetYCoordinate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsMarked() {
+		fail("Not yet implemented");
+	}
+
+}
