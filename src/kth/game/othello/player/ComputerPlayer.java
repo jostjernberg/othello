62ac30ce216c128b320ed607e6748742f7843bf8
Niclas Nilsson <nicke.nisse@gmail.com>
2013-11-08 16:15:02 +0100

    Documentation; Removed author fields.

--- src/kth/game/othello/player/ComputerPlayer.java
+++ src/kth/game/othello/player/ComputerPlayer.java
@@ -2,9 +2,6 @@ package kth.game.othello.player;
 
 /**
  * An immutable PlayerInstance with type Player.Type.COMPUTER.
- * 
- * @author niclas
- * 
  */
 public class ComputerPlayer extends PlayerInstance {
 	public ComputerPlayer(String name) {
--- src/kth/game/othello/player/HumanPlayer.java
+++ src/kth/game/othello/player/HumanPlayer.java
@@ -2,9 +2,6 @@ package kth.game.othello.player;
 
 /**
  * An immutable PlayerInstance with type Player.Type.HUMAN.
- * 
- * @author niclas
- * 
  */
 public class HumanPlayer extends PlayerInstance {
 	public HumanPlayer(String name) {
