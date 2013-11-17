4eff28ad88a96f13dbfb4e9c48b1aca496e7c190
Johan Stjernberg <jostjernberg@gmail.com>
2013-11-17 17:51:27 +0100

    Dev; Added game state enum.

---
+++ src/kth/game/othello/OthelloStates.java
@@ -0,0 +1,15 @@
+package kth.game.othello;
+
+/**
+ * The current game state.
+ */
+public enum OthelloStates {
+	/** Game has not yet started. */
+	STARTUP,
+
+	/** Game is ongoing. */
+	ACTIVE,
+
+	/** Game has ended. */
+	FINISHED;
+}
