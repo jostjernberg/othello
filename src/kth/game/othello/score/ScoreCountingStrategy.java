4cf379aca0878369d2aa06c2bd570e4c9a794a35
Niclas Nilsson <nicke.nisse@gmail.com>
2013-11-21 15:53:56 +0100

    Added methods for score retreival.

--- src/kth/game/othello/score/ScoreCountingStrategy.java
+++ src/kth/game/othello/score/ScoreCountingStrategy.java
@@ -1,18 +1,13 @@
 package kth.game.othello.score;
 
-import kth.game.othello.board.Board;
+import java.util.List;
 
 /**
  * The responsibility of this interface is to supply methods that determine the score for players on a board.
  */
 public interface ScoreCountingStrategy {
 
-	/**
-	 * Calculate the score for the player with playerId on the given board.
-	 * 
-	 * @param board
-	 * @param players
-	 * @return
-	 */
-	public int getPlayerScore(Board board, String playerId);
+	public List<ScoreItem> getPlayersScore();
+
+	public int getPoints(String playerId);
 }
