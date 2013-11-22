44f17054b4ca960067877fd8070288bc6bdefc9f
Niclas Nilsson <nicke.nisse@gmail.com>
2013-11-22 20:39:59 +0100

    Created an implementation of OthelloCreator.

---
+++ src/kth/game/othello/OthelloCreatorImpl.java
@@ -0,0 +1,22 @@
+package kth.game.othello;
+
+import java.util.List;
+
+import kth.game.othello.board.Board;
+import kth.game.othello.player.Player;
+import kth.game.othello.score.ClassicScoreCountingStrategy;
+import kth.game.othello.score.Score;
+import kth.game.othello.score.ScoreImpl;
+
+public class OthelloCreatorImpl implements OthelloCreator {
+
+	@Override
+	public Othello createOthello(Board board, List<Player> players) {
+		Rules rules = new ClassicRules(board);
+		TurnHandler turnHandler = new TurnHandler(rules, players);
+		MoveHandler moveHandler = new MoveHandler(board, rules, turnHandler);
+		Score score = new ScoreImpl(board, ClassicScoreCountingStrategy.INSTANCE);
+
+		return new OthelloImpl(board, players, rules, moveHandler, turnHandler, score);
+	}
+}
