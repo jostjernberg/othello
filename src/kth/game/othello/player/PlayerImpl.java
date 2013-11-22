ef48831383c93001b1164a253375072d568ff3a2
Niclas Nilsson <nicke.nisse@gmail.com>
2013-11-22 20:20:42 +0100

    Implemented Player.

--- src/kth/game/othello/player/PlayerImpl.java
+++ src/kth/game/othello/player/PlayerImpl.java
@@ -1,40 +1,51 @@
 package kth.game.othello.player;
 
+import java.util.UUID;
+
 import kth.game.othello.player.movestrategy.MoveStrategy;
 
 /**
  * An immutable player representation with type Player.Type.COMPUTER.
  */
-class PlayerImpl implements Player {
+public class PlayerImpl implements Player {
+	private UUID id = UUID.randomUUID();
+	private MoveStrategy moveStrategy;
+	private String name;
+	private Type type;
+
+	PlayerImpl(String name, Type type, MoveStrategy moveStrategy) {
+		this(name, type);
+		this.moveStrategy = moveStrategy;
+	}
+
+	PlayerImpl(String name, Type type) {
+		this.name = name;
+		this.type = type;
+	}
 
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
-		return null;
+		return id.toString();
 	}
 
 	@Override
 	public MoveStrategy getMoveStrategy() {
-		// TODO Auto-generated method stub
-		return null;
+		return moveStrategy;
 	}
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 	@Override
 	public Type getType() {
-		// TODO Auto-generated method stub
-		return null;
+		return type;
 	}
 
 	@Override
 	public void setMoveStrategy(MoveStrategy moveStrategy) {
-		// TODO Auto-generated method stub
-
+		this.moveStrategy = moveStrategy;
 	}
 
 }
