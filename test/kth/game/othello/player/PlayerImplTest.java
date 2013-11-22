6ff653a593cdf62bf0e94dc641f79e6a7a7bcd8b
Niclas Nilsson <nicke.nisse@gmail.com>
2013-11-22 20:21:46 +0100

    Created simple unit test for PlayerImpl.

---
+++ test/kth/game/othello/player/PlayerImplTest.java
@@ -0,0 +1,14 @@
+package kth.game.othello.player;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PlayerImplTest {
+
+	@Test
+	public void uniqueIdTest() {
+		Player p1 = new PlayerImpl("player1name", Player.Type.COMPUTER);
+		Player p2 = new PlayerImpl("player2name", Player.Type.COMPUTER);
+		Assert.assertNotEquals(p1.getId(), p2.getId());
+	}
+}
