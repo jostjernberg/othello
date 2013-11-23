9dba983fb86113527db0b477aa7912614c8412e6
Niclas Nilsson <nicke.nisse@gmail.com>
2013-11-23 15:37:46 +0100

    Added documentation to constructor in PlayerImpl.

--- src/kth/game/othello/player/PlayerImpl.java
+++ src/kth/game/othello/player/PlayerImpl.java
@@ -4,15 +4,23 @@ import java.util.UUID;
 
 import kth.game.othello.player.movestrategy.MoveStrategy;
 
-/**
- * An immutable player representation with type Player.Type.COMPUTER.
- */
 public class PlayerImpl implements Player {
 	private UUID id = UUID.randomUUID();
 	private MoveStrategy moveStrategy;
 	private String name;
 	private Type type;
 
+	/**
+	 * Creates a new PlayerImpl.
+	 * 
+	 * @param name
+	 *            Name of the player.
+	 * @param type
+	 *            Type of the player.
+	 * @param moveStrategy
+	 *            This must be set if type == COMPUTER. If type == HUMAN this value will never be used and can thus be
+	 *            null.
+	 */
 	PlayerImpl(String name, Type type, MoveStrategy moveStrategy) {
 		this.name = name;
 		this.type = type;
