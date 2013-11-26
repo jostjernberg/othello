f43d7f2eeb2e45762aaac31a2648e1cb299266c1
Johan Stjernberg <jostjernberg@gmail.com>
2013-11-26 16:13:59 +0100

    Feature; Added Impatient move strategy + test.

---
+++ src/kth/game/othello/player/movestrategy/ImpatientMoveStrategy.java
@@ -0,0 +1,32 @@
+package kth.game.othello.player.movestrategy;
+
+import kth.game.othello.Rules;
+import kth.game.othello.board.Board;
+import kth.game.othello.board.Node;
+
+/**
+ * This MoveStrategy will always make the first legal move.
+ */
+public class ImpatientMoveStrategy implements MoveStrategy {
+	private String name = "Impatient move strategy";
+	public static ImpatientMoveStrategy INSTANCE = new ImpatientMoveStrategy();
+
+	private ImpatientMoveStrategy() {
+
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public Node move(String playerId, Rules rules, Board board) {
+		for (Node n : board.getNodes()) {
+			if (rules.isMoveValid(playerId, n.getId())) {
+				return n;
+			}
+		}
+		return null;
+	}
+}
---
+++ test/kth/game/othello/player/movestrategy/ImpatientMoveStrategyTest.java
@@ -0,0 +1,34 @@
+package kth.game.othello.player.movestrategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kth.game.othello.Rules;
+import kth.game.othello.TestUtil;
+import kth.game.othello.board.Board;
+import kth.game.othello.board.Node;
+import kth.game.othello.player.Player;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ImpatientMoveStrategyTest {
+	TestUtil testUtil = new TestUtil();
+
+	@Test
+	public void canMakeAMoveTest() {
+		List<Player> players = testUtil.mockedDefaultPlayers();
+		Board board = testUtil.mockedDefaultBoard(players);
+		Rules rules = Mockito.mock(Rules.class);
+		List<Node> nodesToSwap = new ArrayList<>();
+		nodesToSwap.add(board.getNode(3, 3));
+		nodesToSwap.add(board.getNode(2, 4));
+		Mockito.when(rules.isMoveValid(Mockito.any(String.class), Mockito.any(String.class))).thenReturn(true);
+
+		ImpatientMoveStrategy ims = ImpatientMoveStrategy.INSTANCE;
+
+		Node move = ims.move(players.get(0).getId(), rules, board);
+		Assert.assertNotEquals(null, move);
+	}
+}
