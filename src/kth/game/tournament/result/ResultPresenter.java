f6da4281172ab0e3de730c46434beaf2057d14f9
Niclas Nilsson <nicke.nisse@gmail.com>
2013-12-08 17:50:39 +0100

    Added documentation.

--- src/kth/game/tournament/result/ResultPresenter.java
+++ src/kth/game/tournament/result/ResultPresenter.java
@@ -2,8 +2,15 @@ package kth.game.tournament.result;
 
 import kth.game.tournament.Tournament;
 
+/**
+ * The responsibility of this entity is to present the results of a tournament.
+ */
 public interface ResultPresenter {
 
+	/**
+	 * Present the results of a tournament.
+	 * @param tournament
+	 */
 	public void present(Tournament tournament);
 
 }
