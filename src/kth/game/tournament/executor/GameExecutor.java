ff69763f346ad25ad1cddc3095aa6680865551f9
Niclas Nilsson <nicke.nisse@gmail.com>
2013-12-08 17:52:08 +0100

    Added documentation.

--- src/kth/game/tournament/executor/GameExecutor.java
+++ src/kth/game/tournament/executor/GameExecutor.java
@@ -1,7 +1,14 @@
 package kth.game.tournament.executor;
 
+/**
+ * The responsibility of classes implementing this interface is to ensure that a game is played from start to end.
+ */
 public interface GameExecutor {
 	
+	/**
+	 * Start the execution of this game.
+	 * @param playerId The player that will make the first move.
+	 */
 	public void start(String playerId);
 	
 }
