1e111ea43b8794f2101d9ce4d02312cf2e77f421
Johan Stjernberg <jostjernberg@gmail.com>
2013-12-08 16:32:07 +0100

    Documentation; Changed incorrect use of javadoc to an ordinary comment.

--- src/kth/game/tournament/Tournament.java
+++ src/kth/game/tournament/Tournament.java
@@ -7,22 +7,21 @@ import kth.game.tournament.result.ResultPresenter;
 public class Tournament {
 	List<TournamentRound> tournamentRounds;
 	ResultPresenter resultPresenter;
-	
+
 	Tournament(List<TournamentRound> tournamentRounds, ResultPresenter resultPresenter) {
 		this.tournamentRounds = tournamentRounds;
 		this.resultPresenter = resultPresenter;
 	}
-	
-	/**
-	 * Assumes that a round is blocking, that is: its start()-method won't return until the game has finished.
-	 */
+
 	public void start() {
-		for(TournamentRound round : tournamentRounds) {
+		// Assumes that a round is blocking, that is: its start()-method won't
+		// return until the game has finished.
+		for (TournamentRound round : tournamentRounds) {
 			round.start();
 		}
 		presentResults();
 	}
-	
+
 	private void presentResults() {
 		resultPresenter.present(tournamentRounds);
 	}
