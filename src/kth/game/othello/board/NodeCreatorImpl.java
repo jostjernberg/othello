f80e6c9a1ef6f12e73b2fa181d26a0aac1b98434
Niclas Nilsson <nicke.nisse@gmail.com>
2013-12-08 20:27:20 +0100

    Added documentation.

--- src/kth/game/othello/board/NodeCreatorImpl.java
+++ src/kth/game/othello/board/NodeCreatorImpl.java
@@ -1,10 +1,13 @@
 package kth.game.othello.board;
 
+/**
+ *	The responsibility of this entity is to implement the NodeCreator interface. 
+ */
 public class NodeCreatorImpl implements NodeCreator {
 	public static NodeCreatorImpl INSTANCE = new NodeCreatorImpl();
 
 	private NodeCreatorImpl() {
-
+		// empty
 	}
 
 	@Override
