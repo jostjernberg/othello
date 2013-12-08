212f1c0659f5987f2a84f9de86f021c3f096b5b1
Niclas Nilsson <nicke.nisse@gmail.com>
2013-12-08 17:54:47 +0100

    Added documentation.

--- src/kth/game/tournament/executor/SilentGameExecutor.java
+++ src/kth/game/tournament/executor/SilentGameExecutor.java
@@ -2,6 +2,10 @@ package kth.game.tournament.executor;
 
 import kth.game.othello.Othello;
 
+/**
+ * The responsibility of this class is to take a Computer vs. Computer othello game and force the players to make moves until the 
+ * game is over.
+ */
 public class SilentGameExecutor implements GameExecutor {
 	Othello othello;
 	
@@ -20,5 +24,4 @@ public class SilentGameExecutor implements GameExecutor {
 			othello.move();
 		}
 	}
-
 }
