484f6ea29e0b0054547d08be8bd74f4a9adeba18
Niclas Nilsson <nicke.nisse@gmail.com>
2013-12-08 20:16:56 +0100

    Added documentation.

--- src/kth/game/othello/player/PlayerCreatorImpl.java
+++ src/kth/game/othello/player/PlayerCreatorImpl.java
@@ -3,12 +3,15 @@ package kth.game.othello.player;
 import kth.game.othello.player.movestrategy.MoveStrategy;
 import kth.game.othello.player.movestrategy.RandomMoveStrategy;
 
+/**
+ * The responsibility of this entity is to implement the PlayerCreator interface.
+ */
 public class PlayerCreatorImpl implements PlayerCreator {
 	public static PlayerCreatorImpl INSTANCE = new PlayerCreatorImpl();
 	private MoveStrategy defaultStrategy = RandomMoveStrategy.INSTANCE;
 
 	private PlayerCreatorImpl() {
-
+		// empty
 	}
 
 	@Override
