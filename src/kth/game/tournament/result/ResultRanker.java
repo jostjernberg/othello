ba945ab63db337a8ebffed88fdb1bd42401e60d4
Niclas Nilsson <nicke.nisse@gmail.com>
2013-12-08 17:48:00 +0100

    Added documentation.

--- src/kth/game/tournament/result/ResultRanker.java
+++ src/kth/game/tournament/result/ResultRanker.java
@@ -5,7 +5,16 @@ import java.util.List;
 import kth.game.othello.player.movestrategy.MoveStrategy;
 import kth.game.tournament.Tournament;
 
+/**
+ * The responsibility of this interface is to rank the strategies in a tournament.
+ */
 public interface ResultRanker {
 	
+	/**
+	 * Rank the strategies in a tournament.
+	 * @param tournament
+	 * @return A sorted list of strategies, where the first element was the best strategy, sorted descendingly.
+	 */
 	public List<MoveStrategy> rankStrategies(Tournament tournament);
+
 }
