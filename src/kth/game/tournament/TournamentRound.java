26aad84693da1a03820d62df9b4fa8e4b18f0d3a
Niclas Nilsson <nicke.nisse@gmail.com>
2013-12-08 18:06:00 +0100

    Added documentation and set private access to instance variables.

--- src/kth/game/tournament/TournamentRound.java
+++ src/kth/game/tournament/TournamentRound.java
@@ -11,10 +11,16 @@ import kth.game.tournament.executor.GameExecutor;
  * The responsibility of this entity is to hold an othello game and a way to execute it, to be used in a tournament.
  */
 public class TournamentRound {
-	Othello othello;
-	GameExecutor gameExecutor;
-	String startingPlayerId;
+	private Othello othello;
+	private GameExecutor gameExecutor;
+	private String startingPlayerId;
 	
+	/**
+	 * Creates a new tournament round.
+	 * @param othello The game that will be played.
+	 * @param gameExecutor The instance that will ensure that all players make moves until the game has ended.
+	 * @param startingPlayerId The player that will begin.
+	 */
 	public TournamentRound(Othello othello, GameExecutor gameExecutor, String startingPlayerId) {
 		this.othello = othello;
 		this.gameExecutor = gameExecutor;
